bd84481bc8d5429260dc6fc6c065ca3c3d37bfc0
grisella <serggood@gmail.com>
2025-02-01 14:06:11 +0300

    parallel

--- src/main/java/org/example/itkissues/ItkIssuesApplication.java
+++ src/main/java/org/example/itkissues/ItkIssuesApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @SpringBootApplication
@@ -12,5 +15,24 @@ public class ItkIssuesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ItkIssuesApplication.class, args);
+
+        List<Student> students = Arrays.asList(
+                new Student("Student1", Map.of("Math", 90, "Physics", 85)),
+                new Student("Student2", Map.of("Math", 95, "Physics", 88)),
+                new Student("Student3", Map.of("Math", 88, "Chemistry", 92)),
+                new Student("Student4", Map.of("Physics", 78, "Chemistry", 85))
+        );
+
+        Map<String, Double> averageGrades = students.parallelStream()
+                .flatMap(student -> student.getGrades().entrySet().stream())
+                .collect(Collectors.groupingBy(
+                        Map.Entry::getKey,
+                        Collectors.averagingInt(Map.Entry::getValue)
+                ));
+
+        System.out.println("Average:");
+        averageGrades.forEach((subject, avgGrade) ->
+                System.out.println(String.format("%s: %.1f", subject, avgGrade))
+        );
     }
 }
---
+++ src/main/java/org/example/itkissues/Student.java
@@ -0,0 +1,18 @@
+package org.example.itkissues;
+
+import java.util.Map;
+
+class Student {
+    private String name;
+    private Map<String, Integer> grades;
+
+    public Student(String name, Map<String, Integer> grades) {
+        this.name = name;
+        this.grades = grades;
+    }
+
+    public Map<String, Integer> getGrades() {
+        return grades;
+    }
+}
+
